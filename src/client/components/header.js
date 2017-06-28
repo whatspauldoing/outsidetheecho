5204f264d0a9d27cac20e702fbe4cef8dae29c4d
Jack Pickett <jack@half-a-second.com>
2017-06-28 22:47:19 +0100

    react-icon in header + responsive

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,11 +1,12 @@
 import { PropTypes } from 'react'
+import FaTwitter from 'react-icons/lib/fa/twitter'
 
 export const Header = ({siteInfo}) => {
   return (
     <div className="header">
       <div className="header__container">
         <div className="header__holder">
-          <h1>{siteInfo.h1}</h1>
+          <h1>{siteInfo.h1} <FaTwitter /></h1>
           <h4>{siteInfo.h4}</h4>
           <p>{siteInfo.intro1}</p>
           <p>{siteInfo.intro2}</p>
